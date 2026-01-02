e9d9174602e86d4669b0efad68a7505e829ceeea
Sikee18 <rathu1829@gmail.com>
2026-01-02 20:12:36 +0530

    SumOfDigits

---
+++ sumofdigits.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class sumofdigits {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        int sum=0;
+        while(num!=0){
+            int rem=num%10;
+            sum+=rem;
+            num=num/10;
+        }System.out.println(sum);
+    }
+
+}
