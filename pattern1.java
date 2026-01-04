4362c9a4b514899f5af02a1a8e0742dfeeb15c49
Sikee18 <rathu1829@gmail.com>
2026-01-04 14:06:23 +0530

    Diamond Pattern

---
+++ pattern1.java
@@ -0,0 +1,25 @@
+public class pattern1 {
+    public static void main(String args[]){
+        int n=5;
+        for(int i=0;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                 System.out.print(" ");
+                }
+            for(int j=1;j<=i;j++){
+                 System.out.print(" *");
+                }
+        System.out.println();
+            }
+        for(int i=4;i>=0;i--){
+            for(int j=1;j<=n-i;j++){
+                 System.out.print(" ");
+                }
+            for(int j=1;j<=i;j++){
+                 System.out.print(" *");
+                }
+        System.out.println();
+            }
+    }
+}
+    
+
