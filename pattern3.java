eea3e167b0c7628c05b509abaedf9ce5581fb0d0
Sikee18 <rathu1829@gmail.com>
2026-01-06 23:14:00 +0530

    pattern 3

--- pattern3.java
+++ pattern3.java
@@ -1,10 +1,10 @@
 public class pattern3 {
     public static void main(String args[]){
         int n=5;
-        for(int rows=1;rows<=n;rows++){
-            for(int cols=1;cols<=(n-rows)+1;cols++){
-                System.out.print(n-rows+1+" ");
-
+        for(int rows=1;rows<=n*2-1;rows++){
+            int rowc=n>=rows?rows:(n*2)-rows;
+            for(int star=1;star<=rowc;star++){
+                System.out.print("*");
             }System.out.println();
         }
     }
