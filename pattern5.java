3831ff1cf0b90af644bfa4c16ef8a6971b154382
Sikee18 <rathu1829@gmail.com>
2026-01-08 23:18:21 +0530

    Number concentric Square

---
+++ pattern5.java
@@ -0,0 +1,17 @@
+public class pattern5 {
+    public static void main(String args[]){
+    int n=5;
+    for(int rows=1;rows<=n*2-1;rows++){
+        for(int cols=1;cols<=n*2-1;cols++){
+            int top=rows;
+            int bottom=(n*2)-rows;
+            int left=cols;
+            int right=(n*2)-cols;
+            int mins=Math.min(Math.min(top,bottom), Math.min(right,left));
+            System.out.print(mins);
+        }System.out.println();
+
+    }
+    
+}
+}
