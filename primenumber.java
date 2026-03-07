ebe1596356f338abc9cf40133440b9f6f78ebe3e
Sikee18 <rathu1829@gmail.com>
2026-03-07 22:28:15 +0530

    primenumber

---
+++ primenumber.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class primenumber {
+public static void main(String args[]){
+    Scanner sc= new Scanner(System.in);
+    int n=sc.nextInt();
+    if(n%2==0){
+    System.out.println("Not prime");
+    }else{
+        System.out.println("Prime");
+    }
+}
+}
