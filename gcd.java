62142b48f29a03293db204fff2cfb8ad4d6b4632
Sikee18 <rathu1829@gmail.com>
2026-03-09 22:30:41 +0530

    lcm

--- gcd.java
+++ gcd.java
@@ -5,7 +5,8 @@ public class gcd {
         int a=sc.nextInt();
         int b=sc.nextInt();
         int gcd=0;
-        for(int i=1;i<Math.min(a,b);i++){
+        int lcm=1;
+        for(int i=1;i<=Math.min(a,b);i++){
             if(a%i==0 && b%i==0){
                 gcd=i;
         }
